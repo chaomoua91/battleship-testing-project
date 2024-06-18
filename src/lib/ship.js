9cebfc73023a3013f797bd18f84f0e782fa154dc
Chao Moua <chaomoua91@gmail.com>
2024-06-17 20:57:21 -0500

    wrote first successful test and function

--- src/lib/ship.js
+++ src/lib/ship.js
@@ -6,4 +6,23 @@ export default class Ship {
     this.#length = length;
     this.#hits = 0;
   }
+
+  //   hit() {
+  //     this.#hits += 1;
+  //   }
+
+  //   /** @returns {boolean} */
+  //   isSunk() {
+  //     return this.#hits >= this.#length;
+  //   }
+
+  //   /** @returns {number} */
+  //   get length() {
+  //     return this.#length;
+  //   }
+
+  //   /** @returns {number} */
+  //   get hits() {
+  //     return this.#hits;
+  //   }
 }
--- src/tests/ship.test.js
+++ src/tests/ship.test.js
@@ -2,8 +2,8 @@ import Ship from "../lib/ship";
 
 describe("Test the ship class", () => {
   test("Ship is created with correct length", () => {
-    const ship1 = new ship(5);
-    const ship2 = new ship(2);
+    const ship1 = new Ship(5);
+    const ship2 = new Ship(2);
 
     expect(ship1.length).toBe(5);
     expect(ship2.length).toBe(2);
