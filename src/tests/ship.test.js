e84e93e8bff1b71ffab990de7f7ed076576e4c11
Chao Moua <chaomoua91@gmail.com>
2024-06-17 20:39:52 -0500

    wrote first test to try

--- src/tests/ship.test.js
+++ src/tests/ship.test.js
@@ -1,1 +1,11 @@
 import Ship from "../lib/ship";
+
+describe("Test the ship class", () => {
+  test("Ship is created with correct length", () => {
+    const ship1 = new ship(5);
+    const ship2 = new ship(2);
+
+    expect(ship1.length).toBe(5);
+    expect(ship2.length).toBe(2);
+  });
+});
