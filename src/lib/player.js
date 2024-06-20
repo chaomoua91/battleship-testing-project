c8c7ba36aed9cff4f8bc991a4cec40e503dbc715
Chao Moua <chaomoua91@gmail.com>
2024-06-20 16:39:35 -0500

    wrote player type in code

--- src/lib/player.js
+++ src/lib/player.js
@@ -1,1 +1,20 @@
 import GameBoard from "./gameboard";
+
+export default class Player {
+  #board;
+
+  #type;
+
+  constructor(type) {
+    this.#board = new GameBoard();
+    this.#type = type;
+  }
+
+  get gameboard() {
+    return this.#board;
+  }
+
+  get type() {
+    return this.#type;
+  }
+}
