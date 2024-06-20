c315d5dd50bbab0e74a69465c9a2db07da058ba4
Chao Moua <chaomoua91@gmail.com>
2024-06-20 16:54:34 -0500

    finished writing code for random placement of ships for the players

--- src/lib/gameUtils.js
+++ src/lib/gameUtils.js
@@ -5,3 +5,19 @@ function getRandomOrientation() {
 
   return index === 0 ? "h" : "v";
 }
+
+export function placeRandom(player) {
+  for (let i = 2; i < 7; i++) {
+    let x = Math.floor(Math.random() * 10) + 1;
+    let y = Math.floor(Math.random() * 10) + 1;
+    let orientation = getRandomOrientation();
+
+    let placementResult = player.gameboard.placeShip([x, y], i, orientation);
+
+    while (!placementResult) {
+      x = Math.floor(Math.random() * 10) + 1;
+      y = Math.floor(Math.random() * 10) + 1;
+      orientation = getRandomOrientation();
+    }
+  }
+}
