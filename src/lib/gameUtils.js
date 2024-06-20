9acfbf44f6abc05d74dda25e6c729293c961f310
Chao Moua <chaomoua91@gmail.com>
2024-06-20 17:03:24 -0500

    wrote function to create grid and add classes for the "human" player

--- src/lib/gameUtils.js
+++ src/lib/gameUtils.js
@@ -21,3 +21,24 @@ export function placeRandom(player) {
     }
   }
 }
+
+export function createGrid(container, player) {
+  const { board } = player.gameboard;
+
+  for (let i = 0; i < 10; i++) {
+    const row = document.createElement("div");
+    row.classList.add("row");
+
+    for (let j = 0; j < 10; j++) {
+      const cell = document.createElement("div");
+      cell.classList.add("cell");
+      if (player.type === "human" && board[i][j]) cell.classList.add("ship");
+
+      cell.dataset.x = i.toString();
+      cell.dataset.y = j.toString();
+
+      row.appendChild(cell);
+    }
+    container.appendChild(row);
+  }
+}
