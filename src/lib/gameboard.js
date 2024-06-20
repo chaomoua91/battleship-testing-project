87e611306aa75160c036dfdcd10ebdbf00bfaf01
Chao Moua <chaomoua91@gmail.com>
2024-06-20 16:14:37 -0500

    wrote code part of the gameboard testing. trying to fix wording on valid placement part

--- src/lib/gameboard.js
+++ src/lib/gameboard.js
@@ -1,1 +1,66 @@
 import Ship from "./ship";
+
+export default class GameBoard {
+  #board;
+
+  #ships;
+
+  #hits;
+
+  constructor() {
+    this.#board = Array.from({ length: 10 }, () =>
+      Array.from({ length: 10 }, () => null)
+    );
+
+    this.#ships = [];
+    this.#hits = new Set();
+  }
+
+  placeShip(coordinates, length, orientation) {
+    if (!this.#isValidPlacement(coordinates, length, orientation)) return false;
+
+    const [x, y] = coordinates;
+    const ship = new Ship(length);
+    this.#ships.push(ship);
+
+    if (orientation === "h") {
+      for (let i = 0; i < length; i++) {
+        this.#board[x][y + i] = ship;
+      }
+    }
+
+    if (orientation === "v") {
+      for (let i = 0; i < length; i++) {
+        this.#board[x + i][y] = ship;
+      }
+    }
+    return true;
+  }
+
+  #isValidPlacement(coordinates, length, orientation) {
+    const [x, y] = coordinates;
+
+    // If starting coordinates are out of gameboard bounds
+    if (x < 0 || x > 9 || y < 0 || y > 9) return false;
+
+    if (orientation === "h") {
+      // If ship would exceed horizontal bounds
+      if (y + length - 1 > 9) return false;
+
+      // If there already is a ship in the new ships path
+      for (let i = 0; i < length; i++) {
+        if (this.#board[x][y + i]) return false;
+      }
+    }
+
+    if (orientation === "v") {
+      if (x + length - 1 > 9) return false;
+
+      for (let i = 0; i < length; i++) {
+        if (this.#board[x + i][y]) return false;
+      }
+    }
+
+    return true;
+  }
+}
