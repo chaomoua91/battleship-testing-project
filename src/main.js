bf97a65f1b82bd54cd313600517cbcd1acbf0382
Chao Moua <chaomoua91@gmail.com>
2024-06-21 14:48:37 -0500

    import functions from other files and code in the variables needed

--- src/main.js
+++ src/main.js
@@ -1,2 +1,12 @@
 import _ from "lodash";
 import "./style.css";
+import Player from "./lib/player";
+import {
+  attackRandom,
+  createGrid,
+  displayWinner,
+  placeRandom,
+} from "./lib/gameUtils";
+
+const buttonContainer = document.querySelector(".start-buttons");
+const restartContainer = document.querySelector(".restart-buttons");
